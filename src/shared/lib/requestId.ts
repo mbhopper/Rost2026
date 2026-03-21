dc2cf2af9dda208b420e055792a6fc44437734aa
mbhopper <an.shteren@gmail.com>
2026-03-22 00:30:59 +0300

    Fix success-flow requestId routing

---
+++ src/shared/lib/requestId.ts
@@ -0,0 +1,15 @@
+export const REQUEST_ID_QUERY_PARAM = 'requestId';
+
+export const buildRequestSuccessPath = (
+  basePath: string,
+  requestId: string,
+) => {
+  const searchParams = new URLSearchParams({
+    [REQUEST_ID_QUERY_PARAM]: requestId,
+  });
+
+  return `${basePath}?${searchParams.toString()}`;
+};
+
+export const getRequestIdFromSearchParams = (searchParams: URLSearchParams) =>
+  searchParams.get(REQUEST_ID_QUERY_PARAM);
